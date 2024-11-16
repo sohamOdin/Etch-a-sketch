bd1e9e510f5675d2bf8464f32953563a6020fee9
Soham <sohamgangurde1@gmail.com>
2024-11-16 23:55:35 +0530

    Make Etch A Sketch
    Learned various concepts along the way like how to implement rgb colors,how to implement a grid using js only,then I also learnt to clear grid using while loop.

--- main.js
+++ main.js
@@ -1,25 +1,65 @@
-const container = document.querySelector("#container");
-let rows = document.getElementsByClassName("rowSketch");
+const CONTAINER = document.querySelector("#container");
+const RESET = document.querySelector("#reset");
+const RGB = document.querySelector("#rgb");
+const CLEAR  = document.querySelector("#clear")
+let color = document.querySelector("#palette");
+let isRGB = false;
 
-function makeGrid(){
-    makeRows(16);
-    makeCols(16);
-}
-
-makeGrid();
-
-function makeRows(numRows){
-    for(let i=0;i<numRows;i++){
-        let box = document.createElement("div");
-        container.appendChild(box).className = "rowSketch";
-    }
-}
-
-function makeCols(numCols){
-    for(let i=0;i<rows.length;i++){
-        for(let j=0;j<numCols;j++){
-            let box = document.createElement("div");
-            rows[j].appendChild(box).className = "colSketch";
+function makeGrid(size){
+    for(let i=0;i<size;i++){
+        let row = document.createElement("div");
+        for(let j=0;j<size;j++){
+            let column = document.createElement("div");
+            column.setAttribute("id","column")
+            column.style.opacity = 0;
+            row.appendChild(column);
+            column.addEventListener('mouseenter',()=>{
+                if(isRGB){
+                    column.style.backgroundColor = randomColor();
+                }else{
+                    column.style.backgroundColor = color.value;
+                }
+                let currentOpacity = parseFloat(column.style.opacity);
+                let newOpacity = Math.min(currentOpacity + 0.1,1);
+                column.style.opacity = newOpacity;
+            });
         }
+        row.setAttribute("id","row");
+        CONTAINER.appendChild(row);
     }
 }
+
+makeGrid(16);
+
+function deleteGrid(){
+    while(CONTAINER.firstChild){
+        CONTAINER.removeChild(CONTAINER.firstChild);
+    }
+}
+
+function randomColor(){
+    let color = [];
+    for(let i=0;i<3;i++){
+        color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(',') + ')';
+}
+
+RGB.addEventListener('click',()=>{
+    isRGB = !isRGB;
+});
+
+RESET.addEventListener('click',()=>{
+    let newSize;
+    deleteGrid();
+    while(true){
+        newSize = +prompt("Enter resolution: ",1);
+        if(newSize>0 && newSize<101) break;
+    }
+    makeGrid(newSize);
+});
+
+CLEAR.addEventListener('click',()=>{
+    deleteGrid();
+    makeGrid(16);
+});
